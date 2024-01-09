a4fbd934096bdfb7cc76cdec6dccbf81cb6129ed
Kamran <kamran@uforiainfotech.com>
2024-01-09 12:16:56 +0500

    - file routing

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,5 @@
 import '@/app/ui/global.css';
-import {inter} from '@/app/ui/fonts';
+import SideNav from "@/app/ui/dashboard/sidenav.tsx";
 
 export default function RootLayout({
   children,
@@ -7,8 +7,11 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <html lang="en">
-      <body className={`${inter.className} antialiased`}>{children}</body>
-    </html>
+      <div className="flex h-screen flex-col md:flex-row md:overflow-hidden">
+        <div className="w-full flex-none md:w-64">
+          <SideNav />
+        </div>
+        <div className="flex-grow p-6 md:overflow-y-auto md:p-12">{children}</div>
+      </div>
   );
 }
